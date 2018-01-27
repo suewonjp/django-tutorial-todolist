ee75049b9e3d7804b1d2d7abb2bb69733fb00b91
suewonjp <suewonjp@gmail.com>
2018-01-27 11:49:00 +0900

    1st commit

---
+++ todos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, re_path
+
+from . import views
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    # re_path(r'^details/(?P<id>\w{0,50})/$', views.details),
+    # re_path(r'^add', views.add, name='add')
+]
+
