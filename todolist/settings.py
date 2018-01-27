98d6b0ad4d8e8844fe68d8acd4c2ddc9a96ba209
suewonjp <suewonjp@gmail.com>
2018-01-27 18:22:46 +0900

    add mysql settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
