83ff33f071d2fe50df2db7538d44479e953ba561
suewonjp <suewonjp@gmail.com>
2018-01-28 18:05:18 +0900

    add views about presenting Todo items

--- todos/urls.py
+++ todos/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    # re_path(r'^details/(?P<id>\w{0,50})/$', views.details),
+    re_path(r'^details/(?P<id>\w{0,50})/$', views.details),
     # re_path(r'^add', views.add, name='add')
 ]
 
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 def index(request):
-    return HttpResponse('Hello World')
+    # return HttpResponse('Hello World')
+    todos = Todo.objects.all()[:10]
+    context = {
+            'todos':todos
+    }
+    return render(request, 'index.html', context)
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+            'todo':todo
+    }
+    return render(request, 'details.html', context)
 
