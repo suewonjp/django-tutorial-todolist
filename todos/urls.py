3a09245b1ca1c65c4b4a91952652621b31149622
suewonjp <suewonjp@gmail.com>
2018-01-28 18:32:19 +0900

    add submit function of Todo items

--- todos/urls.py
+++ todos/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^details/(?P<id>\w{0,50})/$', views.details),
-    # re_path(r'^add', views.add, name='add')
+    re_path(r'^add', views.add, name='add')
 ]
 
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 
@@ -16,4 +16,14 @@ def details(request, id):
             'todo':todo
     }
     return render(request, 'details.html', context)
+
+def add(request):
+    if (request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+        todo = Todo(title=title, text=text)
+        todo.save()
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html')
 
