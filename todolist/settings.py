c471d41524d702c55d2bc7906472fedd06c59f82
suewonjp <suewonjp@gmail.com>
2018-02-02 11:26:43 +0900

    refactor part for serving static files

--- todolist/settings.py
+++ todolist/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+# ]
 
