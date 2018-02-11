4fe23036c9bbe0aa58c442fcb7c68552b74802b2
suewonjp <suewonjp@gmail.com>
2018-02-11 22:53:27 +0900

    fix runtime warning; "DateTimeField received a naive datetime while time zone support is active"

--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return '%s: %s' % (self.id, self.title)
